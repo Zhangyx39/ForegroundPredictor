9bbb41b542f4e36114fcc6d31d00e55d24d2b19a
Tommy <tommykuaidlaa@gmail.com>
2017-11-30 20:08:06 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ForegroundPredictor"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Yixing Zhang on 11/30/17.
+  */
+object Test {
+    def main(args: Array[String]) {
+        println("hello")
+    }
+}
